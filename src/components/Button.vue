48aa5c5de1afda7700fb550b67111436fb0d9287
abhishek kumar <meisabikumar@gmail.com>
2022-10-19 15:34:21 +0530

    .

---
+++ src/components/Button.vue
@@ -0,0 +1,23 @@
+<template lang="">
+    <div>
+        <button @click="onClick()" :style="{background:color}"  class="btn"  >{{text}}</button>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "Button",
+  props: {
+    text: String,
+    color: String,
+  },
+  methods:{
+    onClick(){
+       this.$emit('btn-click')
+    }
+  }
+};
+</script>
+
+<style lang="">
+</style>
---
+++ src/components/Tasks.vue
@@ -0,0 +1,27 @@
+<template>
+  <div :key="task.id" v-for="task in tasks">
+    <Task
+      @toogle-reminder="$emit('toogle-reminder', task.id)"
+      @delete-task="$emit('delete-task', task.id)"
+      :task="task"
+    />
+  </div>
+</template>
+
+<script>
+import Task from "./Task.vue";
+
+export default {
+  name: "Tasks",
+  props: {
+    tasks: Array,
+  },
+  components: {
+    Task,
+  },
+  emits: ["delete-task", "toogle-reminder"],
+};
+</script>
+
+<style  scoped>
+</style>
